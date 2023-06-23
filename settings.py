a08ba4fe5e6a20692ed59ef06a70381c47d535e9
Ilya Malashenko <melax08@yandex.ru>
2023-06-23 14:46:40 +0400

    Add default values to settings in settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 
 class Config(object):
     """Configure settings for flask."""
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                        default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
