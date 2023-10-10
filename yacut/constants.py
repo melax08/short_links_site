618332f89c592e94b81cbc996c6ef3f3c5b1e5d1
Ilya Malashenko <melax08@yandex.ru>
2023-10-10 21:22:14 +0400

    Small refactor and update README.md

--- yacut/constants.py
+++ yacut/constants.py
@@ -1,4 +1,5 @@
+import string
+
 MAX_SHORT_LINK_LEN: int = 16
 RANDOM_SHORT_LINK_LEN: int = 6
-ALLOWED_CHARACTERS: set = set(
-    'jfbmxrqtughwkvzcdionpslaeyJFBMXRQTUGHWKVZCDIONPSLAEY0123456789')
+ALLOWED_CHARACTERS: set = set(string.ascii_letters + string.digits)
